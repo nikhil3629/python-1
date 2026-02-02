c1f13d04b68205417244724692f700e21c7f2954
Satrasala Nikhil Kumar <126808578+nikhil3629@users.noreply.github.com>
2026-02-02 16:54:08 +0530

    Add files via upload

---
+++ ifelse/7.py
@@ -0,0 +1,6 @@
+num = int(984)
+
+if(num%10 == 4):
+    print(" 4 is at units place")
+else:
+    print(" 4 is not at units place")
---
+++ ifelse/8.py
@@ -0,0 +1,6 @@
+num = int(input())
+
+if(num>99 and num<1000):
+    print("num is 3 digit")
+else:
+    print("num is not 3 digit")
